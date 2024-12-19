b189ef5766ecd3f5117da090b426e7267a89e270
Mukarram ali khan <168542386+maksoft121@users.noreply.github.com>
2024-12-19 16:44:10 +0530

    app updated

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!'
+    return "Hello, World!"
 
-if __name__ == '__main__':
-    app.run()
-
+if _name_ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
